6804e567295c363c3263b296aff8f36ab6548e70
Batoul <102414385+Batoul447@users.noreply.github.com>
2022-05-20 22:23:58 +0300

    Adding more styling to the game using javascript

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded",function(){
   let score = 0;
-
   document.getElementById("start").onclick = function(){clicked()};
   
   function clicked(){
@@ -8,6 +7,8 @@ document.addEventListener("DOMContentLoaded",function(){
     score = 0;
     document.getElementById("status").innerHTML = "The game started!";
     item = document.getElementsByClassName("boundary example");
+    item = document.getElementsByClassName("boundary example");
+
     item[0].innerHTML = "Score = "+score;
     item[0].innerHTML = "score = "+score;
 
@@ -19,44 +20,62 @@ document.addEventListener("DOMContentLoaded",function(){
   function functiondiv0(){
     //console.log("YOU are over div 0! ");
     document.getElementById("status").innerHTML = "You lost!";
+    item = document.getElementsByClassName("boundary example");
+
+    item[0].style.backgroundColor = 'red';
     score -=10;
     item[0].innerHTML = "score = "+score;
-
   }
 
   list[1].onmouseover = function () {functiondiv1()}
   function functiondiv1(){
     //console.log("YOU are over div 1! ");
     document.getElementById("status").innerHTML = "You lost!";
+    item = document.getElementsByClassName("boundary example");
+
+    item[0].style.backgroundColor = 'red';
+
     score -= 10;
     item[0].innerHTML = "score = "+score;
-
   }
 
   list[2].onmouseover = function () {functiondiv2()}
   function functiondiv2(){
     //console.log("YOU are over div 2! ");
     document.getElementById("status").innerHTML = "You lost!";
+    item = document.getElementsByClassName("boundary example");
+
+    item[0].style.backgroundColor = 'red';
+
     score -= 10;
     item[0].innerHTML = "score = "+score;
-
   }
 
   list[3].onmouseover = function () {functiondiv3()}
   function functiondiv3(){
     //console.log("YOU are over div 3! ");
     document.getElementById("status").innerHTML = "You lost!";
+    item = document.getElementsByClassName("boundary example");
+
+    item[0].style.backgroundColor = 'red';
+
     score -= 10;
     item[0].innerHTML = "score = "+score;
 
   }
 
   list[4].onmouseover = function () {functiondiv4()}
+
   function functiondiv4(){
     //console.log("YOU are over div 4! ");
     document.getElementById("status").innerHTML = "You lost!";
+    item = document.getElementsByClassName("boundary example");
+
+    item[0].style.backgroundColor = 'red';
+
     score -= 10;
     item[0].innerHTML = "score = "+score;
+
   }
   var end = document.getElementById("end");
   end.onmouseover = function (){won()}
@@ -68,7 +87,7 @@ document.addEventListener("DOMContentLoaded",function(){
     item[0].innerHTML = "score = "+score;
     console.log(score);
     item[0].style.backgroundColor = 'yellow';
- 
+    
   }
     
 });
